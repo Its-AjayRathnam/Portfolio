f65a01e42f35cd0330fe42b9ceba256ebf5556ec
Ajay Rathnam <its.ajayrathnam@gmail.com>
2024-08-05 00:55:38 +0530

    vue config changes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
+const path = require('path');
+
 module.exports = {
+    configureWebpack: {
+        resolve: {
+          alias: {
+            '@': path.resolve(__dirname, 'src'),
+          },
+        },
+      },
     publicPath: process.env.NODE_ENV === 'production'
       ? '/Portfolio/' // For project pages
       : '/'
