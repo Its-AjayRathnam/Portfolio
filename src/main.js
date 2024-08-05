b8f37900ffbcc8c16427ee75acf11d8297637bde
Ajay Rathnam <ajay@descpro.com>
2024-08-05 10:54:55 +0530

    main js changes

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import { registerPlugins } from './plugins'
 import App from './App'
 // Components
 // Composables
-import { createApp } from 'vue'
+import { createApp } from "vue"
 import router from '../src/router/index'
 
 const app = createApp(App)
