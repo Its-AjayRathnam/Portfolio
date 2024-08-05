12df7e51fb7c9743e7e9b02161b9a4424a84d710
Ajay Rathnam <ajay@descpro.com>
2024-08-05 12:00:40 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@
 import { createApp } from "vue"
 import App from './App.vue'
 import { registerPlugins } from './plugins'
-import router from '@/router/index'
+import router from './router/index'
 
 const app = createApp(App)
 
