2f0e2b3bfed8df910d3a1789f9b3e03269adf15b
Ajay Rathnam <its.ajayrathnam@gmail.com>
2024-08-06 22:34:15 +0530

    changes

--- vite.config.mjs
+++ vite.config.mjs
@@ -13,7 +13,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/',
+  base: '/Portfolio',
   build: {
     rollupOptions: {
       output: {
